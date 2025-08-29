d83955e67d6f3459f3741c6d1922966ae56a9260
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2025-08-29 18:02:38 -0300

    fix: testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -32,7 +32,11 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(helmet());      
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false, // desactiva CORP para tu API
+  })
+);    
 app.use(compression()); 
 
 //-- COOKIE PARSER --//
