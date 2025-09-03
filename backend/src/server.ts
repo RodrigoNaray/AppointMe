9442a346b34f5eef7dec0ee40d800daf3abf8513
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2025-09-02 21:26:29 -0300

    fix: test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,8 +24,9 @@ const PORT: string | number = process.env.PORT || 5000;
 
 app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173', 
-
   credentials: true, // Si necesitas enviar cookies o cabeceras de autorización
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
 }));
 
 
@@ -35,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   helmet({
-    crossOriginResourcePolicy: false, // desactiva CORP para tu API
+    crossOriginResourcePolicy: { policy: "cross-origin" },
   })
 );    
 app.use(compression()); 
