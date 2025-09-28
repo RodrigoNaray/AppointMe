244bac95f17f6c0560df3cc93bdbae02ab02592a
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2025-09-28 18:41:53 -0300

    fix: Lazy loading added to admin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
+import {lazy} from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/admin/dashboard/DashboardPage';
-import ServicesPage from './pages/admin/services/ServicesPage';
 import PublicLayout from './layouts/PublicLayout';
-import AdminLayout from './layouts/AdminLayout';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFoundPage from './pages/NotFoundPage';
 import ServicesListPage from './pages/ServicesListPage';
 import ContactPage from './pages/ContactPage';
-import AvailabilityPage from './pages/admin/AvailabilityPage';
-import BookingsPage from './pages/admin/bookings/BookingsPage';
-import ProfilePage from './pages/admin/ProfilePage';
-import SettingsPage from './pages/admin/SettingsPage';
+
+//--- Admin routes ---//
+const AdminLayout = lazy(() => import('./layouts/AdminLayout'))
+const DashboardPage = lazy(() => import('./pages/admin/dashboard/DashboardPage'))
+const ServicesPage = lazy(() => import('./pages/admin/services/ServicesPage'))
+const AvailabilityPage = lazy(() => import('./pages/admin/AvailabilityPage'))
+const BookingsPage = lazy(() => import('./pages/admin/bookings/BookingsPage'))
+const ProfilePage = lazy(() => import('./pages/admin/ProfilePage'))
+const SettingsPage = lazy(() => import('./pages/admin/SettingsPage'))
+//-------------------//
 
 const router = createBrowserRouter([
   {
