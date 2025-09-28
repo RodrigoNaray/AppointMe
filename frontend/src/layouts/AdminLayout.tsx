bc9b520909ec185306a3f6ad0fa447e8eb4e5729
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2025-09-28 17:53:16 -0300

    fix: Name fix

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -18,7 +18,7 @@ export default function AdminLayout() {
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/admin" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span>AppointMe</span>
+              <span>AppointMePro</span>
             </Link>
           </div>
           <div className="flex-1">
--- frontend/src/layouts/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout.tsx
@@ -20,7 +20,7 @@ export default function PublicLayout() {
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span>AppointMe</span>
+              <span>AppointMePro</span>
             </Link>
           </div>
           <div className="flex-1">
